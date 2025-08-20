1902a72eda255e761c97616db0e8123d528dd536
austinxduong <austinxduong@gmail.com>
2025-08-19 21:31:19 -0700

    create login/auth routes

---
+++ server/routes/auth.js
@@ -0,0 +1,15 @@
+import express from "express"
+
+const router = express.Router();
+
+router.post("/login", (req, res, next) => {
+    res.send("login router")
+});
+
+router.post("/register", (req, res, next) => {
+    res.send("register router")
+});
+
+router.post("/logout", (req, res, next) => {
+    res.send("logout router")
+});
